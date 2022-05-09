8a0046ac51564be7034658b3c04997e1f066734a
Alexei Scheglov <aleshascheglov@yandex.ru>
2022-05-10 00:52:42 +0300

    fix: первое ревью

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,19 @@ const popupCloseButton = popup.querySelector('.popup__close');
 const popupEditButton = popup.querySelector('.popup__button-save')
 
 const formElement = popup.querySelector('.popup__form');
-const nameInput = popup.querySelector('#nameInput');
-const jobInput = popup.querySelector('#jobInput');
+const nameInput = popup.querySelector('.popup__input_name');
+const jobInput = popup.querySelector('.popup__input_job');
 
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 
 function popupOpenToggle() {
     popup.classList.toggle('popup_is-opened');
+    if (popup.classList.contains('popup_is-opened'))
+    {
+        nameInput.value = profileTitle.textContent;
+        jobInput.value = profileDescription.textContent;
+    }
 }
 
 function formSubmitHandler (evt) {
@@ -20,10 +25,10 @@ function formSubmitHandler (evt) {
     let job = jobInput.value;
     profileTitle.textContent = name;
     profileDescription.textContent = job;
+    popupOpenToggle();
 }
 
 popupOpenButton.addEventListener('click', popupOpenToggle);
 popupCloseButton.addEventListener('click', popupOpenToggle);
-popupEditButton.addEventListener('click', popupOpenToggle);
 
 formElement.addEventListener('submit', formSubmitHandler);
